4f56946b4895fb40a8df6dbc3677c0210944bc69
Joseph Sismar <josephsismart@gmail.com>
2021-11-16 13:40:13 +0800

    updated excel

--- application/views/interface/useradmin/layout/Report_header.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<address style="text-align:left;border-bottom:2px solid green;margin-bottom:5px;padding-bottom:5px;">
-    Republic of the Philippines<br>
-    <strong>CITY DISASTER RISK REDUCTION AND MANAGEMENT OFFICE</strong><br>
-    City Hall Bldg., J.P. Rosales Ave., Doongan, Butuan City
-</address>
